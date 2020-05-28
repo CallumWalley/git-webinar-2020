51bc8531869fad0392b9e5cc19aa89870b3c8c0a
Callum Walley <callum.walley@nesi.org.nz>
2020-05-28 12:21:49 +1200

    First commit

---
+++ useful_functions.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+
+import datetime
+
+__author__ = "Megan"
+__copyright__ = "Copyright 2020, Callum Walley"
+__credits__ = ["Callum Walley"]
+__license__ = "GPL"
+__version__ = "1.2.6"
+__maintainer__ = "Callum Walley"
+__email__ = "callum.walley@nesi.org.nz"
+
+# Important functions for all of my important work.
+
+
+def get_first_index_of_array(array):
+    """
+    this function is very usefull useful for returning the first index of array.
+    """
+    return array[1]
+
+
+def print_but_louder(in_string):
+    """
+    For when you need to emphisise something.
+   """
+    out_string = in_string.upper() + "!!!!"
+    return out_string
+
+
+def is_it_wednesday():
+    if datetime.datetime.today().weekday() == 2:
+        return True
+    return False
